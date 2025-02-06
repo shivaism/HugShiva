d025d612e03c4e598d7312b5557633c86aece9ca
shivaism <63526897+shivaism@users.noreply.github.com>
2025-02-06 17:04:30 +0700

    Update base.ts

--- packages/wrappers/src/base.ts
+++ packages/wrappers/src/base.ts
@@ -143,8 +143,8 @@ export class BaseWrapper {
     );
 
     let response = useProxy
-      ? uFetch(url, {
-          dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
+      ? fetch(url, {
+//          dispatcher: new ProxyAgent(Settings.ADDON_PROXY),
           method: 'GET',
           headers: headers,
           signal: AbortSignal.timeout(this.indexerTimeout),
